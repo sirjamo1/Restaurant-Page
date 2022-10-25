21dd5e20c9522fe6bfe7b48e2317d0cd1710d449
Sirjamo1 <jam_how@hotmail.com>
2022-10-25 13:09:20 +1000

    home page complete also added white marble text to header

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,10 +1,25 @@
 const home = () => {
-const homeContainer = document.createElement("div");
-homeContainer.classList.add("home-container");
-const homeText = document.createElement("h1");
-homeText.innerHTML = "HOME"
-homeContainer.appendChild(homeText)
-return homeContainer
-}
+    const homeContent = {
+        title: "Marble Steakhouse",
+        description:
+            "Home to four of the best cuts of meat you'll ever eat. </br> </br> Have it your way! We take pride in cooking your steak the way you like it, if you're not 100% satisfied with how it's cooked, we'll cook another. No questions asked.",
+            image: "/src/images/steak-on-grill.png"
+    };
+    const homeContainer = document.createElement("div");
+    const steakOnGrillImg = document.createElement("img");
+    steakOnGrillImg.setAttribute("src", homeContent.image);
+    steakOnGrillImg.setAttribute("alt", "steak on grill");
+    homeContainer.appendChild(steakOnGrillImg);
+    homeContainer.classList.add("home-container");
+    const homeTextContainer = document.createElement("div")
+    const homeTitle = document.createElement("h1");
+    homeTitle.innerHTML = homeContent.title;
+    homeTextContainer.appendChild(homeTitle);
+    const homeDescription = document.createElement("h2");
+    homeDescription.innerHTML = homeContent.description;
+    homeTextContainer.appendChild(homeDescription)
+    homeContainer.appendChild(homeTextContainer)
+    return homeContainer;
+};
 
-export default home
+export default home;
