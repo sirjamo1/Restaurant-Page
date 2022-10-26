0e0e03d85ae8d65e6383c65d3c73c5ecdbd20f24
Sirjamo1 <jam_how@hotmail.com>
2022-10-26 13:07:33 +1000

    added to readme

--- src/footer.js
+++ src/footer.js
@@ -1,10 +1,21 @@
 const footer = () => {
-const footerContainer = document.createElement("footer");
-const footerText = document.createElement("h4")
-footerText.innerHTML = "Built by James Howells"
-footerContainer.appendChild(footerText)
-return footerContainer
-}
+    // let githubIcon = "/src/icons/github.svg";
+    const footerContainer = document.createElement("footer");
+    const footerText = document.createElement("h4");
+    footerText.innerHTML = "Built by James Howells";
 
+    footerContainer.appendChild(footerText);
+    const linkToGithub = document.createElement("a");
+    linkToGithub.setAttribute(
+        "href",
+        "https://github.com/sirjamo1/Restaurant-Page"
+    );
+    const githubIcon = document.createElement("img");
+    githubIcon.setAttribute("src", "/src/icons/github.svg");
+    githubIcon.setAttribute("alt", "github icon");
+    linkToGithub.appendChild(githubIcon);
+    footerText.appendChild(linkToGithub);
+    return footerContainer;
+};
 
-export default footer
+export default footer;
